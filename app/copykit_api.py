ddca511ae63ddbb1849376e982cebdeee2ba19c3
Иванов Александр Ильич <alexanderivanov@MacBook-Pro-Ivanov.local>
2026-04-06 21:27:59 +0300

    new generate_all_apifunction

--- app/copykit_api.py
+++ app/copykit_api.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI
-from typing import Optional
 from copykit import generate_snippets, generate_keywords
 
 app = FastAPI()
 
+# uvicorn copykit_api:app --reload
+# http://127.0.0.1:8000/docs
 
 @app.get("/generate_snippets")
-async def generate_snippet_api(promt: Optional[str] = None):
-    snippet = generate_snippets(promt)
+async def generate_snippet_api(promt: str = None):
+    snippet = await generate_snippets(promt)
 
-    return {"snippet": snippet}
+    return {"snippet": snippet,
+            "keywords": [],
+            "promt": promt}
 
 @app.get("/generate_keywords")
-async def generate_keywords_api(promt: Optional[str] = None):
-    keywords = generate_keywords(promt)
-    return {"keywords": keywords}
+async def generate_keywords_api(promt: str = None):
+    keywords = await generate_keywords(promt)
+
+    return {"snippet": None,
+            "keywords": keywords,
+            "promt": promt}
+
+@app.get("/generate_all")
+async def generate_all_api(promt: str = None):
+    snippets = await generate_snippets(promt)
+    keywords = await generate_keywords(promt)
+
+    return {"snippets": snippets,
+            "keywords": keywords,
+            "promt": promt}
